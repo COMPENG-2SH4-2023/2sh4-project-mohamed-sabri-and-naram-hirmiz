56bd3baed2a1476cec3f071e0889231cd8cee5e0
Naram Hirmiz <hirmin1@mcmaster.ca>
2023-12-03 02:53:30 -0500

    food files

---
+++ Food.cpp
@@ -0,0 +1,27 @@
+#include "Food.h"
+#include "MacUILib.h"
+
+// objPos Food::foodPos()
+// {
+
+// }
+
+Food::Food()
+{
+
+}
+
+Food::~Food()
+{
+
+}
+
+// void Food::generateFood(blockoff:objPos)
+// {
+
+// }
+
+// void Food::getFoodPos(returnPos:objPos&)
+// {
+
+// }
