29ffa364d27e4b2f3c319f2fb4e60515a94627d3
Naram Hirmiz <hirmin1@mcmaster.ca>
2023-12-02 20:31:45 -0500

    1b 75% done

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -2,52 +2,57 @@
 
 GameMechs::GameMechs()
 {
-
+    exitFlag = false;
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
-
+    exitFlag = false; 
+    boardSizeX = boardX; 
+    boardSizeY = boardY;
 }
 
 // do you need a destructor?
-
+GameMechs::~GameMechs()
+{
+//     Perform cleanup if needed
+}
 
 
 bool GameMechs::getExitFlagStatus()
 {
-
+    return exitFlag;
 }
 
 char GameMechs::getInput()
 {
-
+    return input;
 }
 
 int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
-
+    exitFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    input = this_input;
 }
 
 void GameMechs::clearInput()
 {
-
+    input = 0;
 }
 
 
--- Project.cpp
+++ Project.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include "MacUILib.h"
 #include "objPos.h"
+#include "GameMechs.h"  
 
 
 using namespace std;
 
 #define DELAY_CONST 100000
 
-const int width = 30;
-const int height = 15;
-bool exitFlag;
+
+// all global variabes to be removed
+GameMechs* gameMechsPtr;
+
+// const int width = 30;
+// const int height = 15;
+// bool exitFlag;
 
 void Initialize(void);
 void GetInput(void);
@@ -25,7 +30,7 @@ int main(void)
 
     Initialize();
 
-    while(exitFlag == false)  
+    while(!gameMechsPtr->getExitFlagStatus())  
     {
         GetInput();
         RunLogic();
@@ -43,17 +48,83 @@ void Initialize(void)
     MacUILib_init();
     MacUILib_clearScreen();
 
-    exitFlag = false;
+    gameMechsPtr = new GameMechs(30, 15);
+
+    //exitflag = false
 }
 
 void GetInput(void)
 {
-   
+    char input;
+    
+    if (MacUILib_hasChar())
+    {
+        input = MacUILib_getChar();
+        gameMechsPtr->setInput(input); 
+    }
 }
 
 void RunLogic(void)
 {
-    
+    char input = gameMechsPtr->getInput();
+/*
+    if (input != 0) // if not null character
+    {
+        switch (input)
+        {
+            case '/': // exit
+                gameMechsPtr->setExitTrue();
+                break; 
+
+            case 'w':
+                if (movement != DOWN)
+                {
+                    movement = UP;
+                }
+                break;
+
+            case 's':
+                if (movement != UP)
+                {
+                    movement = DOWN;
+                }
+                break;
+
+            case 'a':
+                if (movement != right)
+                {
+                    movement = left;
+                }
+                break;
+
+            case 'd':
+                if (movement != left)
+                {
+                    movement = right;
+                }
+                break;
+
+            default:
+                break;
+
+            case '=':
+                if (speed < 5)
+                {
+                    speed++;
+                }
+                break;
+
+            case '-':
+                if (speed > 0)
+                {
+                    speed--;
+                }                
+                break;
+        }
+
+        gameMechsPtr->clearInput();
+    }
+*/
 }
 
 void DrawScreen(void)
@@ -62,19 +133,19 @@ void DrawScreen(void)
     objPos map(0, 0, '#');
 
     // Draw the top and bottom borders
-   for(int i =0; i<height; i++){
-    for(int j = 0; j<width; j++){
+   for(int i =0; i<gameMechsPtr->getBoardSizeY(); i++){
+    for(int j = 0; j<gameMechsPtr->getBoardSizeX(); j++){
         if(i == 0){
             map.setObjPos(i, 0, '#');
         }
-        else if(i == height -1){
-            map.setObjPos(i, width-1, '#');
+        else if(i ==  gameMechsPtr->getBoardSizeY() -1){
+            map.setObjPos(i,  gameMechsPtr->getBoardSizeX()-1, '#');
         }
         else if(j == 0){
             map.setObjPos(0, j, '#');
         }
-        else if(j == width -1){
-            map.setObjPos(height-1,j,'#');
+        else if(j ==  gameMechsPtr->getBoardSizeX() -1){
+            map.setObjPos( gameMechsPtr->getBoardSizeY()-1,j,'#');
         }else{
             map.setObjPos(i,j,' ');
         }
@@ -94,6 +165,6 @@ void LoopDelay(void)
 void CleanUp(void)
 {
     MacUILib_clearScreen();    
-  
+    delete gameMechsPtr;
     MacUILib_uninit();
 }
