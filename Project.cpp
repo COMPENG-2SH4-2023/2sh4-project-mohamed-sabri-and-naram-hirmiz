5bb51c5fd9c31f937a1d8de887f7dab124f5cfd9
Mohamed Sabri <sabrim2@mcmaster.ca>
2023-12-03 23:56:34 -0500

    Project2

--- Project.cpp
+++ Project.cpp
@@ -143,8 +143,7 @@ void CleanUp(void)
 
 void PrintEnd(void)
 {
-    MacUILib_Delay(500000000);
     MacUILib_printf("Game Over! You Lose!\n");
-    MacUILib_Delay(500000000);
+    MacUILib_Delay(600000000);
 
 }
